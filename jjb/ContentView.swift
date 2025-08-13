9cea3fcfb7ab40bc7c7a53e911841bb0f6170072
nonodu13009 <jeanminono13@gmail.com>
2025-08-13 08:54:53 +0200

    Initial Commit

---
+++ jjb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jjb
+//
+//  Created by Jean-Michel Nogaro on 13/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jjb/jjbApp.swift
@@ -0,0 +1,17 @@
+//
+//  jjbApp.swift
+//  jjb
+//
+//  Created by Jean-Michel Nogaro on 13/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct jjbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
